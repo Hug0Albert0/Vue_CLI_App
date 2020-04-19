58c60e7e4d21f3717112bc9168940b56a148bfb0
Hug0Albert0 <hugorivera.cool.player@gmail.com>
2020-04-19 06:25:21 -0500

    Parámetros dinámicos y rutas a través de eventos

--- src/views/Fotos.vue
+++ src/views/Fotos.vue
@@ -1,6 +1,11 @@
 <template>
    <div>
       <h1>Galeria de Fotos</h1>
+      <router-link
+         :to="{name: 'Fotos', params: {id: item}}"
+         v-for=" (item, index) of fotosArreglo" :key="index">
+         <button>Foto: {{item}}</button>
+      </router-link>
       <Fotografia></Fotografia>
    </div>
 </template>
@@ -12,6 +17,11 @@ import Fotografia from "@/components/Fotografia.vue"
 export default {
    components: {
       Fotografia
+   },
+   data() {
+      return {
+         fotosArreglo: [1, 2, 3, 4, 5]
+      }
    }
 }
 </script>
